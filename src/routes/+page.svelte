c2100dc4d05357a0ec63ce607d0db9a56a966c5b
Azuriru <jeannelee823@gmail.com>
2023-11-19 01:11:47 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<script>
+    import FontAwesome from '$lib/components/FontAwesome.svelte';
+</script>
+
+<div class="home">
+    Stuff
+</div>
+
+<style lang="scss">
+</style>
